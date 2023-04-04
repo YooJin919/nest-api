9cc240a777e30e110112890c3467be4ff55c47ab
Yoo Jin <dbwls30131@ajou.ac.kr>
2023-04-04 17:29:02 +0900

    fix: post.entity typeorm decorator 수정

--- src/post/entity/post.entity.ts
+++ src/post/entity/post.entity.ts
@@ -1,7 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Posts {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
   author: number;
-  thumbnail: string[];
+
+  @Column()
+  thumbnail: string;
+
+  @Column()
   createdAt: Date;
 }
